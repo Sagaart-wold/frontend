fa075ba84e956cad68e9721c79a358811797ae56
Hiperiosity <anastasy_ya@mail.ru>
2024-07-03 09:05:03 +0300

    change package

--- README.md
+++ README.md
@@ -1,30 +1,42 @@
-# React + TypeScript + Vite
+# API Șagaart
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+_MVP сервис онлайн-продажи и оценки предметов искусства. Хакатон_
 
-Currently, two official plugins are available:
+### Основные технологии:
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+React 
+ReactDOM 
+Redux 
+Toolkit 
+React Hook Form
 
-## Expanding the ESLint configuration
+### Стилизация:
 
-If you are developing a production application, we recommend updating the configuration to enable type aware lint rules:
+Material-UI
+Styled Components
 
-- Configure the top-level `parserOptions` property like this:
+### Скрипты:
 
-```js
-export default {
-  // other rules...
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json', './tsconfig.node.json'],
-    tsconfigRootDir: __dirname,
-  },
-}
+Vite
+TypeScript
+
+
+## Запуск:
+
+- Скопировать проект на компьютер
 ```
-
-- Replace `plugin:@typescript-eslint/recommended` to `plugin:@typescript-eslint/recommended-type-checked` or `plugin:@typescript-eslint/strict-type-checked`
-- Optionally add `plugin:@typescript-eslint/stylistic-type-checked`
-- Install [eslint-plugin-react](https://github.com/jsx-eslint/eslint-plugin-react) and add `plugin:react/recommended` & `plugin:react/jsx-runtime` to the `extends` list
+*git clone git@github.com:Sagaart-wold/frontend.git
+```
+- Перейти в папку проекта 
+```
+*cd frontend
+```
+- Установить зависимости
+```
+*npm i
+```
+- Запустить
+```
+*npm run build 
+*npm run dev
+```
--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,15 @@
 import { useState } from "react";
-
 import { Header } from "@components/Header";
-
 import { Main } from "@components/Main";
-
-// import styles from "./index.module.css";
 import { ThemeProvider } from "@mui/material/styles";
-// import responsiveTheme from "./components/theme/mainTheme.tsx";
 import darkTheme from "./components/theme/darkTheme.tsx";
 import lightTheme from "./components/theme/lightTheme.tsx";
-
-import { Test } from "@components/Test";
-
-// import Dialog from "@mui/material/Dialog";
-// import DialogActions from '@mui/material/DialogActions';
-// import DialogContent from '@mui/material/DialogContent';
-// import DialogContentText from '@mui/material/DialogContentText';
-// import DialogTitle from '@mui/material/DialogTitle';
-// import { Test } from "@components/Test";
 
 function App() {
   const [theme, setTheme] = useState<object>(darkTheme);
   const [isShowSearchSection, setIsShowSearchSection] = useState<boolean>(
     false
-  ); //false
+  );
   // Единый стейт для боковых попапов
   const [openPopup, setOpenPopup] = useState<string | null>(null);
 
@@ -46,22 +32,17 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <main>
-        
-          <Header
-            handleOpenPopup={handleOpenPopup}
-            handleClosePopup={handleClosePopup}
-            openPopup={openPopup}
-            isShowSearchSection={isShowSearchSection}
-            handleOpenSearchPopup={handleOpenSearchPopup}
-          />
-          {/* <Login /> */}
-          <Main
-            isShowSearchSection={isShowSearchSection}
-            handleOpenSearchPopup={handleOpenSearchPopup}
-          />
-          {/* <Test /> */}
-
-        
+        <Header
+          handleOpenPopup={handleOpenPopup}
+          handleClosePopup={handleClosePopup}
+          openPopup={openPopup}
+          isShowSearchSection={isShowSearchSection}
+          handleOpenSearchPopup={handleOpenSearchPopup}
+        />
+        <Main
+          isShowSearchSection={isShowSearchSection}
+          handleOpenSearchPopup={handleOpenSearchPopup}
+        />
       </main>
     </ThemeProvider>
   );
